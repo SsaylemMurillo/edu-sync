99522ce0b439780f72a31f1605e35b18081b75cb
SSAYKO <sandresmurillo17@gmail.com>
2023-11-20 20:01:05 -0500

    update/fix: it is confirmed work on login styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Logo from './components/Logo'
 
 // font of the app
 const inter = Inter({ subsets: ['latin'] })
@@ -17,7 +18,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='flex flex-col flex-auto w-full h-screen'> 
+          <div className="h-full">
+            <div className="grid lg:grid-cols-3 h-full">
+              <div className="bg-yellow-500 items-center lg:flex inline-grid shadow-2xl rounded-xl justify-center hidden ">
+                <div className="bg-white rounded-full p-10 bg-center shadow-lg shadow-yellow-800/50 ">
+                  <Logo />
+                </div>
+              </div>
+              <div className="col-span-2 flex justify-center items-center">
+                <div className="min-w-[450] px-8">
+                  <div className="mb-8">
+                    <h1 className="text-3xl font-bold">Bienvenido a EduSync</h1>
+                    <p className="text-xl">Por favor, digite los datos necesarios para iniciar sesión</p><br />
+                    {children}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
 'use client';
 
+import { useState } from "react";
+
 export default function Home() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div className="">Login - EduSync</div>
+    <div>
+        <div className='mb-4'>
+          <label className='font-medium text-justify text-l mb-1 flex'>Correo Electronico</label>
+          <input id='email' value={email} required type='email' onChange={(e) => {setEmail(e.target.value)}} placeholder='Escriba su Correo Electronico' className='w-full border rounded-md bg-transparent border-gray-400 p-2.5' />
+        </div>
+        <div className='mb-4'>
+          <label className='font-medium text-justify text-l mb-1 flex'>Contraseña</label>
+          <input id='password' value={password} onChange={(e) => {setPassword(e.target.value)}}  required type='password' placeholder='Escriba su Contraseña' className='w-full border rounded-md bg-transparent border-gray-400 p-2.5' />
+        </div>
+
+        <div className="mt-5 flex flex-col gap-y-5">
+          <button type='submit'
+            className="hover:bg-yellow-600 transition-all py-3 rounded-md bg-yellow-500 text-white text-l font-semibold">
+            Iniciar Sesion </button>
+        </div>
+      </div>
   )
 }
